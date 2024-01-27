f71f9fed0dbc491261f908d1a936f108d8143912
FoxStamp <26rgreenman@athenian.org>
2024-01-27 11:40:59 -0800

    Updated dataset, refactored processing of data

--- processData.py
+++ processData.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-csv_name = "Human Randomness_ Patterns and Predictability in Participant-Generated Number Sequences (Responses) - Dataset 1 (incomplete).csv"
-df = pd.read_csv(csv_name)
+csv_name = "Human Randomness_ Patterns and Predictability in Participant-Generated Number Sequences (Responses) - 50 count.csv"
+human_df = pd.read_csv(csv_name)
 
-subset_df = df.iloc[:, 1:51]
-count_data = subset_df.apply(lambda x: x.value_counts()).fillna(0)
+subset_human_df = human_df.iloc[:, 1:51]
 
-count_data.plot(kind='bar', stacked=True, legend=False)
 
 plt.xlabel('Number')
 plt.ylabel('Count')
--- processDatav2.py
+++ processDatav2.py
@@ -1,13 +1,30 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-csv_name = "Human Randomness_ Patterns and Predictability in Participant-Generated Number Sequences (Responses) - Dataset 1 (incomplete).csv"
-df = pd.read_csv(csv_name)
+#open human CSV
+csv_name = "Human Randomness_ Patterns and Predictability in Participant-Generated Number Sequences (Responses) - 50 count.csv"
+human_df = pd.read_csv(csv_name)
 
-nums_df = df.iloc[:, 1:51]
+#select only the numbers
+subset_human_df = human_df.iloc[:, 1:51]
 
-print(nums_df)
+fig, ax = plt.subplots(5, 2)
 
-# nums_dict = {}
-# for i in range(10):
-#     nums_dict[i] = 0
+#for numbers 0-9 count each
+for i in range(10):
+    place = int(i/2), i%2
+
+    counts = np.zeros(10)
+
+    for _, row in subset_human_df.iterrows():
+        for j in range(50 - 1):
+            if row[j] == i:
+                counts[row[j + 1]] += 1
+
+    ax[place].bar(range(10), counts, color='green')
+    ax[place].set_title(f"Nums succeeding {i}")
+
+
+plt.tight_layout()
+plt.show()
